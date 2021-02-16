91649fbd6cbc3af7fa8be2fff7dbf01634cc63d3
FasePlay <67728870+FasePlay@users.noreply.github.com>
2021-02-16 18:26:31 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let blockDiv = document.querySelector('#text');
+let svgimg = document.querySelector('#svg_img');
+
+svgimg.onclick = function() {
+    blockDiv.textContent += 'Чо зыришь? Кивика в кураторы бери!     ';
+}
